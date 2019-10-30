2ffd8d9705ef50aed93709b6791cd1763176da9c
Mobasshir Bhuiyan <31741188+bhuiyanmobasshir94@users.noreply.github.com>
2019-10-30 15:12:40 +0600

    edited with new algorithm

--- Problem-Solving/diagonal_difference.py
+++ Problem-Solving/diagonal_difference.py
@@ -6,7 +6,15 @@ for i in range(a.shape[0]):
     for j in range(a.shape[1]):
         if i == j:
             l_d += a[i][j]
-        if j == (a.shape[1] - i):
+        if j == (a.shape[1] - i - 1):
             r_d += a[i][j]
 output = abs(l_d-r_d)
 print(output)
+
+# ans = 0
+# for i in range(len(c)):
+#     ans += c[i][i]
+#     ans -= c[i][len(c)-1-i]
+    
+# ans
+    
